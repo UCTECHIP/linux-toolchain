7c8291ac8f4050af0c689784feef4c532497ffea
yexc <xc.ye@uctechip.com>
2019-10-11 10:19:28 +0800

    add prebuild GNU toolchain for RV32 Linux

---
+++ riscv32-uctechip-linux-gnu-gcc-8.3.0-2019.09-x86_64-linux-ubuntu18/lib/gcc/riscv32-uctechip-linux-gnu/8.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/viking/Tools/riscv-gnu-toolchain/riscv-gcc/configure --target=riscv32-uctechip-linux-gnu --prefix=/home/viking/OS/Linux/prefix_imac --with-sysroot=/home/viking/OS/Linux/prefix_imac/sysroot --with-system-zlib --enable-shared --enable-tls --enable-languages=c,c++,fortran --disable-libmudflap --disable-libssp --disable-libquadmath --disable-nls --disable-bootstrap --src=.././riscv-gcc --enable-checking=yes --disable-multilib --with-abi=ilp32 --with-arch=rv32imac --with-tune=rocket 'CFLAGS_FOR_TARGET=-O2  -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-O2  -mcmodel=medlow'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32" }, { "arch", "rv32imac" }, { "tune", "rocket" } };
